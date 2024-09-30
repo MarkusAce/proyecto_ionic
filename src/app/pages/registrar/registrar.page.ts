06430c9729093ca42e2e29b33bbb5c6df9b03128
Markus <marcoantonioroblesgarrido@gmail.com>
2024-09-30 17:43:17 -0300

    registro cambiado reactive form

--- src/app/pages/registrar/registrar.page.ts
+++ src/app/pages/registrar/registrar.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, ValidationErrors, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registrar',
@@ -9,48 +9,98 @@ import { AlertController } from '@ionic/angular';
 })
 export class RegistrarPage implements OnInit {
 
-  usuario1: string = "";
-  email1: string = "";
-  contrasena1: string = "";
-  contrasenarepetida: string = "";
+  registroForm!: FormGroup;
 
-  constructor(private router: Router,private alertController: AlertController) { }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
+
+  
 
   ngOnInit() {
+    this.registroForm = this.formBuilder.group({
+      usuario1: ['', [Validators.required, Validators.minLength(5)]],
+      email1: ['', [Validators.required, Validators.email]],
+      rut:['', [Validators.required, this.rutValidador]],
+      telefono: ['',[Validators.required, Validators.pattern(/^[0-9]{9,10}$/)]],
+      direccion: ['', [Validators.required,Validators.minLength(5)]],
+      fechanac: ['', [Validators.required, this.fechaNacValidar]],
+      contrasena1: [null, [Validators.required, this.contrasenaValidador]],
+      contrasenarepetida: ['', Validators.required]
+    }, {validator: this.contrasenasIguales});
+
   }
 
-  async presentAlert(titulo: string, msj: string) {
-    const alert = await this.alertController.create({
-      header: titulo,
-      message: msj,
-      buttons: ['OK']
-    });
-    await alert.present();
+  contrasenaValidador(control: AbstractControl): ValidationErrors | null {
+    const contrasena = control.value;
+    const errors: ValidationErrors = {};
+
+    if(!contrasena){
+      errors['vacia'] = true;
+      return errors;
+    }
+    const mayusculaValid = /[A-Z]/.test(contrasena);
+    const minusculaValid = /[a-z]/.test(contrasena);
+    const numeroValid = /\d/.test(contrasena);
+    const especialValid = /[@$!%*?&]/.test(contrasena);
+    const minimoValid = contrasena.length >=8;
+
+    if(!mayusculaValid){
+      errors['mayuscula'] = true;
+    }
+    if (!minusculaValid){
+      errors['minuscula'] = true;
+    }
+    if (!numeroValid){
+      errors['numero'] = true;
+    }
+    if (!especialValid){
+      errors['especial'] = true;
+    }
+    if (!minimoValid){
+      errors['minimo'] = true;
+    }
+    return Object.keys(errors).length > 0 ? errors : null;
+  }
+
+  contrasenasIguales(form: FormGroup) {
+    const contrasena = form.get('contrasena1')?.value;
+    const contrasenarepetida = form.get('contrasenarepetida')?.value;
+    return contrasena === contrasenarepetida? null : {mismatch: true};
+  }
+
+  rutValidador(control: AbstractControl): ValidationErrors | null {
+    const rut = control.value;
+    
+    const rutLimpio = rut.replace(/[^0-9kK]/g, '');
+
+    const rutRegex = /^[0-9]{7,8}[0-9kK]?$/;
+
+    if (!rutRegex.test(rutLimpio)){
+      return {rutInvalido: true}
+    }
+    return null;
+  }
+
+  fechaNacValidar(control: AbstractControl): ValidationErrors | null {
+    const fechaNac = new Date(control.value);
+    const fechaActual = new Date();
+    const edadMinima = 16;
+    const edadMaxima = 130;
+    const edadUsuario = fechaActual.getFullYear() - fechaNac.getFullYear();
+
+    if (edadUsuario < edadMinima){
+      return {menorDeEdad: true};
+    }
+    if (edadUsuario > edadMaxima){
+      return {maximoEdad: true};
+    }
+    if (fechaNac >= fechaActual){
+      return {fechaInvalida: true};
+    }
+    return null;
   }
 
   validarRegistro(){
-
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-    const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if ((this.usuario1 =="") || (this.email1 =="") || (this.contrasena1 =="") || (this.contrasenarepetida =="")){
-      this.presentAlert('Error','Los campos no pueden estar vacios.')
-    }
-    else if((this.usuario1 =="MarkusAce")|| (this.usuario1 =="NicolasMa")){
-      this.presentAlert('Error','El nombre de usuario ya existe.')
-    }
-    else if(!correoRegex.test(this.email1)){
-      this.presentAlert('Error','El email no es válido.')
-    }
-    else if(!passwordRegex.test(this.contrasena1)){
-      this.presentAlert('Error', 'La contraseña ingresada no puede tener menos de 8 caracteres, debe contener una mayuscula, un numero y un caracter especial Ej: @')
-    }
-    else if(this.contrasenarepetida != this.contrasena1){
-      this.presentAlert('Error','Las contraseñas ingresadas no coinciden.')
-    }
-    else{
-      this.presentAlert('Exito','Usted se ha registrado exitosamente.')
+    if (this.registroForm.valid){
       this.router.navigate(['/login']);
     }
   }
